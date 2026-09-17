b63547296893f38c7409a413811cf0342a985f13
MwKun <matwon713@gmail.com>
2026-09-17 18:35:06 -0400

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -4,27 +4,31 @@ class Solution:
         tCount = {}
 
         for char in s:
-            if char in sCount:
-                sCount[char] += 1
-            else:
+            if char not in sCount:
                 sCount[char] = 1
-
-        for char in t:
-            if char in tCount:
-                tCount[char] += 1
             else:
+                sCount[char] += 1
+        
+        for char in t:
+            if char not in tCount:
                 tCount[char] = 1
+            else:
+                tCount[char] += 1
+
 
         if len(sCount) > len(tCount):
-            for sCountKey in sCount:
-                if not (sCountKey in tCount):
+            for char, count in sCount.items():
+                if char not in tCount:
                     return False
-                if sCount[sCountKey] != tCount[sCountKey]:
+                if tCount[char] != count:
                     return False
         else:
-            for tCountKey in tCount:
-                if not (tCountKey in sCount):
+            for char, count in tCount.items():
+                if char not in sCount:
                     return False
-                if sCount[tCountKey] != tCount[tCountKey]:
+                if sCount[char] != count:
                     return False
+        
+
         return True
+        
