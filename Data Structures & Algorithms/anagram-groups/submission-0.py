68703710f4d6b4081d85ccbb8062ed1b59f7af9e
MwKun <matwon713@gmail.com>
2026-09-17 18:54:18 -0400

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.py
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -1,17 +1,40 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hasht = {}
-        anagrams = []
 
-        if (len(strs) == 1):
+    # def isAnagram(s: str, t: str) -> bool:
+    #     sAnagram = {}
+    #     tAnagram = {}
+
+    #     if len(s) != len(t):
+    #         return False
+
+    #     for i in range(len(s)):
+    #         sAnagram[s[i]] = 1 + sAnagram.get(s[i], 0)
+    #         tAnagram[t[i]] = 1 + tAnagram.get(t[i], 0)
+
+    #     return sAnagram == tAnagram
+
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 0:
+            return []
+        elif len(strs) == 1:
             return [[strs[0]]]
 
-        for s in strs:
-            item = "".join(sorted(s))
-            if item in hasht:
-                anagrams[hasht[item]].append(s)
-            else:
-                hasht[item] = len(anagrams)
-                anagrams.append([s])
+        existAnagrams = {}
 
-        return anagrams
+        for i in range(len(strs)):
+            sortedString = "".join(sorted(strs[i]))
+            if sortedString not in existAnagrams:
+                existAnagrams[sortedString] = [strs[i]]
+            else:
+                existAnagrams[sortedString].append(strs[i])
+        
+        arrayYuh = []
+
+        for _, listItem in existAnagrams.items():
+            arrayYuh.append(listItem)
+
+        return arrayYuh
+
+                
+            
