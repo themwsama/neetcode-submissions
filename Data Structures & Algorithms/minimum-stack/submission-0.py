9ba086e96bd54df124241e49ecd548b0e6fed5e0
MwKun <matwon713@gmail.com>
2026-09-17 21:35:23 -0400

    Update: minimum-stack - submission-0

--- Data Structures & Algorithms/minimum-stack/submission-0.py
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -1,24 +1,26 @@
 class MinStack:
 
     def __init__(self):
-        self.values = []
-        self.min_stack = []  # Tracks the minimum value at each state
+        self.array = []
+        self.minStack = []
+        self.min = None
 
     def push(self, val: int) -> None:
-        self.values.append(val)
-        # If min_stack is empty or val is smaller/equal to current min, push it
-        if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)
+        self.array.append(val)
+        value = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(value)
 
     def pop(self) -> None:
-        if self.values:
-            # If the value being popped is the current minimum, pop it from min_stack too
-            if self.values[-1] == self.min_stack[-1]:
-                self.min_stack.pop()
-            self.values.pop()
+        self.array.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
-        return self.values[-1]
+        if len(self.array) == 0:
+            return None
+        return self.array[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.minStack[-1]
+        
+
+        
